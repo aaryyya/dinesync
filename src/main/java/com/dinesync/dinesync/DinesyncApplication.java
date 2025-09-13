872af9bb2b51d9351f2fdb3762a8628f9b719ebe
aaryyya <aryampatil2005@gmail.com>
2025-09-13 18:08:55 +0530

    Initial Commit

---
+++ src/main/java/com/dinesync/dinesync/DinesyncApplication.java
@@ -0,0 +1,13 @@
+package com.dinesync.dinesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinesyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinesyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinesync/dinesync/DinesyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesync.dinesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinesyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
