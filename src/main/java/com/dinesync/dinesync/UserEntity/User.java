8fb707ccc49b6a53e8ad793f5551d3a7436b64e5
aaryyya <aryampatil2005@gmail.com>
2025-10-15 23:20:40 +0530

    Added Lombok

--- src/main/java/com/dinesync/dinesync/UserEntity/User.java
+++ src/main/java/com/dinesync/dinesync/UserEntity/User.java
@@ -1,45 +1,41 @@
 package com.dinesync.dinesync.UserEntity;
 import jakarta.persistence.*;
+import lombok.*;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String userName;
     private String email;
-
-    public User() {}
-
-    public User(Long id, String userName, String email) {
-        this.id = id;
-        this.userName = userName;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
+//    public User() {}
+//    public User(Long id, String userName, String email) {
+//        this.id = id;
+//        this.userName = userName;
+//        this.email = email;
+//    }
+//    public Long getId() {
+//        return id;
+//    }
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//    public String getUserName() {
+//        return userName;
+//    }
+//    public void setUserName(String userName) {
+//        this.userName = userName;
+//    }
+//    public String getEmail() {
+//        return email;
+//    }
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
